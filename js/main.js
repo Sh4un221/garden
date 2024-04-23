b325d26a92df9f404cae58017af061ccf2318454
Sh4un221 <127805282+Sh4un221@users.noreply.github.com>
2024-04-23 12:31:06 -0500

    feat: :construction: Se implemento la consulta 5

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@ import {
 
 import{
     getAllNameSurnamesAndEmailInCargeOfBossSeven,
+    getBossesFullnameAndEmail,
     getAll
 } from "./module/employees.js"
 // console.log(await getAllOfficesCodeAndCity());
 // console.log(await getAll());
 // console.log(await getAllOfficesAndPhonesFromSpain());
 // console.log(await getAllNameSurnamesAndEmail());
+// console.log(await getBossesFullnameAndEmail());
 console.log(await getAll());
--- js/module/employees.js
+++ js/module/employees.js
@@ -4,26 +4,44 @@ export const getAllNameSurnamesAndEmailInCargeOfBossSeven = async () => {
     let data = await res.json();
     let dataUpdate = [];
     data.forEach(val=>{
+        let[email]=val.email.match(/(?<=\[)[^\[\]]+@[^@\[\]]+(?=\])/)
         dataUpdate.push({
             nombre:val.name,
             apellidos:val.lastname1+' '+val.lastname2,
-            email:val.email
+            email
         })
     })
     return dataUpdate;
 }
 // 4. Devuelve el nombre del puesto, nombre, apellidos y email del jefe de la empresa.
-export const getAll= async () => {
-    let res = await fetch("http://localhost:5502/employees?position=Director General")
+export const getBossesFullnameAndEmail= async () => {
+    let res = await fetch("http://localhost:5502/employees")
     let data = await res.json();
     let dataUpdate = [];
     data.forEach(val=>{
-        dataUpdate.push({
-            puesto: val.position,
-            nombre:val.name,
-            apellidos:val.lastname1+' '+val.lastname2,
-            email:val.email
-        })
+        let[email]=val.email.match(/(?<=\[)[^\[\]]+@[^@\[\]]+(?=\])/)
+        if (val.code_boss==null){
+            dataUpdate.push({
+                puesto: val.position,
+                nombre:val.name,
+                apellidos:val.lastname1+' '+val.lastname2,
+                email
+            })
+        }
     })
     return dataUpdate;
+}
+// 5. Devuelve un listado con el nombre, apellidos y puesto de aquellos empleados que no sean representantes de ventas.
+export const getAll=async()=>{
+    let res=await fetch("http://localhost:5502/employees?position_ne=Representante Ventas")
+    let data=await res.json();
+    let dataUpdate=[];
+    data.forEach(val=>{
+        dataUpdate.push({
+            nombre:val.name,
+            apellidos:`${val.lastname1} ${val.lastname2}`,
+            puesto:val.position
+        })
+    })
+    return dataUpdate
 }
